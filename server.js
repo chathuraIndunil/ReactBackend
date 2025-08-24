574d4c18a81e3ac7bca85153a6244c299a1ffab6
chathuraIndunil <chathuraindunil807@gmail.com>
2025-08-25 00:16:25 +0530

    createServerFile

---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+const port=3001;
+const host ='127.0.0.1';
+const server = app.listen(3001,'127.0.0.1',() =>{
+    console.log('Node server is listning to ${server.address().port}')
+});
