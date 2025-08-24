f90422108acdf6af9f409cfdd89d879404abe061
chathuraIndunil <chathuraindunil807@gmail.com>
2025-08-25 00:22:08 +0530

    runNodemon

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
-const port=3001;
-const host ='127.0.0.1';
-const server = app.listen(3001,'127.0.0.1',() =>{
-    console.log('Node server is listning to ${server.address().port}')
+const port = 3001;
+const host = '127.0.0.1';
+
+const server = app.listen(port, host, () => {
+    console.log(`Node server is listening on port ${server.address().port}`);
 });
